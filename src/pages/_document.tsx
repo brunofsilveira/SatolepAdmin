dc218d947cfcb55b98a9983a48fb07e449189fd1
Bruno Ferraz Silveira <bfssfb@outlook.com.br>
2021-08-21 03:55:58 -0300

    🎨 Add src directory

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,5 @@
-import theme from '@app/common/theme'
 import { ColorModeScript } from '@chakra-ui/react'
+import theme from '@common/theme'
 import Document, {
   DocumentContext,
   Head,
@@ -16,8 +16,13 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang='en'>
-        <Head />
+      <Html lang='pt-br'>
+        <Head>
+          <link
+            rel='stylesheet'
+            href='https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap'
+          />
+        </Head>
         <body>
           <ColorModeScript
             initialColorMode={theme.config.initialColorMode}
